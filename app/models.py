41cf012d4d79cb3ee5286b1ba1859f7b519f1a58
shaowei <shaowei@shouxin168.com>
2019-07-09 16:10:24 +0800

    motify

--- app/models.py
+++ app/models.py
@@ -13,7 +13,6 @@ class User(AbstractUser):
 
     class Meta:
         db_table = 'user'
-        ordering = ["-id"]
 
 
 class Store(models.Model):
@@ -34,4 +33,112 @@ class Store(models.Model):
 
     class Meta:
         db_table = 'store'
+
+
+class ProductCategory(models.Model):
+    """产品类目表"""
+    title = models.CharField(max_length=255, blank=True, null=True, verbose_name="产品类目标题")
+    url = models.CharField(max_length=255, blank=True, null=True, verbose_name="产品类目标题url")
+    category_id = models.CharField(db_index=True, max_length=255,blank=True, null=True, verbose_name="产品类目id")
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING, blank=True, null=True)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        # managed = False
+        unique_together = ("category_id", "store")
+        db_table = 'product_category'
+
+
+class Product(models.Model):
+    """产品表"""
+    sku = models.CharField(db_index=True, max_length=255, verbose_name="产品标识符")
+    url = models.CharField(max_length=255, blank=True, null=True, verbose_name="产品URL")
+    uuid = models.CharField(max_length=64, verbose_name="产品唯一标识")
+    name = models.CharField(db_index=True, max_length=255, verbose_name="产品名称")
+    image_url = models.CharField(max_length=255, verbose_name="图片URL")
+    thumbnail = models.TextField(verbose_name="缩略图", blank=True, null=True, default=None)
+    price = models.CharField(max_length=255, verbose_name="产品价格")
+    product_category = models.ForeignKey(ProductCategory, on_delete=models.DO_NOTHING,blank=True, null=True)
+    tag = models.CharField(max_length=255, verbose_name="所属标签")
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
+    publish_time = models.DateTimeField(blank=True, null=True, verbose_name="发布时间")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+    url_with_utm = models.CharField(db_index=True, blank=True, null=True, max_length=255, verbose_name=u"产品的带utm构建的url")
+
+    class Meta:
+        # managed = False
+        unique_together = ("product_category", "uuid")
+        db_table = 'product'
         ordering = ["-id"]
+
+
+class EmailGroup(models.Model):
+    """邮件组"""
+    title = models.CharField(db_index=True, max_length=255, verbose_name="标题")
+    description = models.TextField(blank=True, null=False, verbose_name="描述")
+    open_rate = models.CharField(blank=True, null=True, max_length=255, verbose_name="打开邮件比例")
+    click_rate = models.CharField(blank=True, null=True, max_length=255, verbose_name="单机比例")
+    members = models.CharField(blank=True, null=True, max_length=255, verbose_name="数量")
+    trigger_info = models.TextField(blank=True, null=False, verbose_name="trigger关系")
+    email_delay = models.TextField(blank=True, null=False, verbose_name="发送邮件顺序")
+    customer_group_list = models.TextField(blank=True, null=False, verbose_name="邮件对应的客户组列表")
+    send_rule = models.TextField(blank=True, null=False, verbose_name="发送邮件规则")
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        # managed = False
+        db_table = 'email_group'
+
+
+class EmailInfo(models.Model):
+    """邮件Info"""
+    subject = models.TextField(blank=True, null=False, max_length=255, verbose_name="邮件标题")
+    heading_text = models.TextField(blank=True, null=False, verbose_name="邮件")
+    logo = models.TextField(blank=True, null=False, verbose_name="邮件logo")
+    banner = models.TextField(blank=True, null=False, verbose_name="邮件banner")
+    headline = models.TextField(blank=True, null=False, verbose_name="邮件headline")
+    body_text = models.TextField(blank=True, null=False, verbose_name="邮件body_text")
+    product_list = models.TextField(blank=True, null=False, verbose_name="产品列表")
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        # managed = False
+        db_table = 'email_info'
+
+
+class CustomerGroup(models.Model):
+    """客户组"""
+    title = models.CharField(db_index=True, max_length=255, verbose_name="标题")
+    description = models.TextField(blank=True, null=False, verbose_name="描述")
+    open_rate = models.CharField(blank=True, null=True, max_length=255, verbose_name="打开邮件比例")
+    click_rate = models.CharField(blank=True, null=True, max_length=255, verbose_name="单机比例")
+    members = models.CharField(blank=True, null=True, max_length=255, verbose_name="数量")
+    relation_info = models.TextField(blank=True, null=False, verbose_name="客户关系")
+    customer_list = models.TextField(blank=True, null=False, verbose_name="对应客户列表")
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        # managed = False
+        db_table = 'customer_group'
+
+
+class Customers(models.Model):
+    """客户表"""
+    name = models.CharField(max_length=255, verbose_name="客户名称")
+    email = models.EmailField(max_length=255, blank=True, null=True, verbose_name="客户邮箱")
+    register_time = models.DateTimeField(blank=True, null=True, verbose_name="客户注册时间")
+    last_cart_time = models.DateTimeField(blank=True, null=True, verbose_name="最近购物时间")
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        db_table = 'customers'
--- app/urls.py
+++ app/urls.py
@@ -1,2 +1,26 @@
 from django.conf.urls import url, include
 from app.views import shopify_auth, personal_center
+
+
+auth_urlpatterns = [
+
+    url(r'shopify/callback/$', shopify_auth.ShopifyCallback.as_view()),
+    url(r'shopify/ask_permission/$', shopify_auth.ShopifyAuthView.as_view()),
+]
+
+
+v1_urlpatterns = [
+
+    # 注册 登陆
+    url(r'^login/$', personal_center.LoginView.as_view()),
+    # shopfy注册设置密码-->注册
+    url(r'^set_password/(?P<pk>[0-9]+)/$', personal_center.SetPasswordView.as_view()),
+    # 登陆状态下设置密码
+    url(r'^set_passwords/(?P<pk>[0-9]+)/$', personal_center.SetPasswordsView.as_view()),
+
+]
+
+urlpatterns = [
+    url(r'^v1/auth/', include(auth_urlpatterns)),
+    url(r'^v1/', include(v1_urlpatterns)),
+]
