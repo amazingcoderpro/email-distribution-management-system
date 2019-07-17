909c963c6654a1dc23a43c9c39d880f49c0dfece
shaowei <shaowei@shouxin168.com>
2019-07-17 14:11:27 +0800

    modify

--- app/views/webhook.py
+++ app/views/webhook.py
@@ -49,21 +49,17 @@ class EventOrderPaid(APIView):
 
     def post(self, request, *args, **kwargs):
         print("------------ order paid ------------:")
-        store_url = request.META["HTTP_X_SHOPIFY_SHOP_DOMAIN"]
-        event_uuid = request.data["id"]
-        order_uuid = request.data[""]
-
-        print(request.META, type(request.META))
-        print(json.dumps(request.data))
+        res = {}
+        res["store_url "] = request.META["HTTP_X_SHOPIFY_SHOP_DOMAIN"]
+        res["order_uuid"] = request.data["id"]
+        res["status"] = 1
+        res["customer_uuid"] = request.data["customer"]["id"]
+        li = []
+        for item in request.data["line_items"]:
+            product_id = item["product_id"]
+            title = item["title"]
+            price = item["price"]
+            quantity = item["quantity"]
+            li.append({"product_id":product_id,"title":title,"price":"price","quantity":quantity})
+        models.OrderEvent.objects.create(**res)
         return Response({"code": 200})
-
-
-    # event_uuid = models.CharField(max_length=255, verbose_name="事件的唯一标识符")
-    # order_uuid = models.CharField(max_length=255, verbose_name="订单的唯一标识符")
-    # status = models.IntegerField(default=0, verbose_name="订单事件类型, 0-创建(未支付)，1-支付")
-    # store_url = models.CharField(max_length=255, verbose_name="订单对应的店铺的url")
-    # customer = models.CharField(max_length=255, db_index=True, verbose_name="订单对应客户id")
-    #
-    # # [{"product": "123456", "sales": 2, "amount": 45.22}, {"product": "123456", "sales": 1, "amount": 49.22}]
-    # products = models.TextField(blank=True, null=True, verbose_name="订单所涉及到的产品及其销量信息")
-    # create_time = models.DateTimeField(auto_now=True, db_index=True, verbose_name="订单创建时间")
