1014288dc3b9cd222232eda9a6635110fa575949
shaowei <shaowei@shouxin168.com>
2019-07-18 15:30:24 +0800

    modify

--- app/views/webhook.py
+++ app/views/webhook.py
@@ -52,7 +52,8 @@ class EventOrderCreate(APIView):
         res["order_uuid"] = request.data["id"]
         res["status"] = 0
         res["total_price"] = request.data["total_price"]
-        res["order_create_time"] = request.data["created_at"].replace("T"," ")[:-6]
+        create_time = request.data["created_at"].replace("T", " ")[:-6]
+        res["order_create_time"] = datetime.datetime.strptime(create_time, "%Y-%m-%d %H:%M:%S")
         res["customer_uuid"] = request.data["customer"]["id"]
         li = []
         for item in request.data["line_items"]:
@@ -72,13 +73,16 @@ class EventOrderCreate(APIView):
             customer_res["first_name"] = request.data["customer"]["first_name"]
             customer_res["last_name"] = request.data["customer"]["last_name"]
             customer_res["accept_marketing_status"] = request.data["customer"]["accepts_marketing"]
-            customer_res["sign_up_time"] = request.data["customer"]["created_at"].replace("T"," ")[:-6]
+            sign_up_time = request.data["customer"]["created_at"].replace("T"," ")[:-6]
+            customer_res["sign_up_time"] = datetime.datetime.strptime(sign_up_time, "%Y-%m-%d %H:%M:%S")
             customer_res["last_order_status"] = 0
-            customer_res["last_order_time"] = datetime.datetime.now()
+            updated_at = request.data["customer"]["updated_at"].replace("T"," ")[:-6]
+            customer_res["last_order_time"] = datetime.datetime.strptime(updated_at, "%Y-%m-%d %H:%M:%S")
             models.Customer.objects.create(**customer_res)
         else:
             customer_instance.last_order_status = 0
-            customer_instance.last_order_time = datetime.datetime.now()
+            updated_at = request.data["customer"]["updated_at"].replace("T", " ")[:-6]
+            customer_instance.last_order_time = datetime.datetime.strptime(updated_at, "%Y-%m-%d %H:%M:%S")
             customer_instance.save()
         return Response({"code": 200})
 
@@ -105,6 +109,7 @@ class EventOrderPaid(APIView):
             quantity = item["quantity"]
             li.append({"product_id":product_id, "title":title, "price":price, "quantity":quantity})
         order_instance.product_info = json.dumps(li)
-        order_instance.order_update_time = request.data["updated_at"].replace("T"," ")[:-6]
+        updated_at = request.data["customer"]["updated_at"].replace("T", " ")[:-6]
+        order_instance.order_update_time = datetime.datetime.strptime(updated_at, "%Y-%m-%d %H:%M:%S")
         order_instance.save()
         return Response({"code": 200})
