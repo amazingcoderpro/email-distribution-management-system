7bd8ee981103051dd4e67838d881fc6f07779f1d
Silence-python <1447385994@qq.com>
2019-07-18 10:52:04 +0800

    shopify data

--- app/views/webhook.py
+++ app/views/webhook.py
@@ -43,6 +43,38 @@ class EventOrderCreate(APIView):
         print("------------ order create ------------:")
         print(request.META, type(request.META))
         print(json.dumps(request.data))
+        res = {}
+        store = models.Store.objects.filter(url=request.META["HTTP_X_SHOPIFY_SHOP_DOMAIN"]).first()
+        res["store"] = store
+        res["order_uuid"] = request.data["id"]
+        res["status"] = 0
+        res["customer_uuid"] = request.data["customer"]["id"]
+        li = []
+        for item in request.data["line_items"]:
+            product_id = item["product_id"]
+            title = item["title"]
+            price = item["price"]
+            quantity = item["quantity"]
+            li.append({"product_id": product_id, "title": title, "price": price, "quantity": quantity})
+        res["product_info"] = str(li)
+        models.OrderEvent.objects.create(**res)
+        customer_instance = models.Customer.objects.filter(uuid=request.data["customer"]["id"]).first()
+        if not customer_instance:
+            customer_res = {}
+            customer_res["store"] = store
+            customer_res["uuid"] = request.data["customer"]["id"]
+            customer_res["customer_email"] = request.data["customer"]["email"]
+            customer_res["first_name"] = request.data["customer"]["first_name"]
+            customer_res["last_name"] = request.data["customer"]["last_name"]
+            customer_res["accept_marketing_status"] = request.data["customer"]["accepts_marketing"]
+            customer_res["sign_up_time"] = request.data["customer"]["created_at"]
+            customer_res["last_order_status"] = 0
+            customer_res["last_order_time"] = datetime.datetime.now()
+            models.Customer.objects.create(**customer_res)
+        else:
+            customer_instance.last_order_status = 0
+            customer_instance.last_order_time = datetime.datetime.now()
+            customer_instance.save()
         return Response({"code": 200})
 
 
@@ -63,7 +95,7 @@ class EventOrderPaid(APIView):
             title = item["title"]
             price = item["price"]
             quantity = item["quantity"]
-            li.append({"product_id":product_id,"title":title,"price":price,"quantity":quantity})
+            li.append({"product_id":product_id, "title":title, "price":price, "quantity":quantity})
         res["product_info"] = str(li)
         models.OrderEvent.objects.create(**res)
         customer_instance = models.Customer.objects.filter(uuid=request.data["customer"]["id"]).first()
