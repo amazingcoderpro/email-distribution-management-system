73a1d2b8072bed773960efd111c646b4669b8778
Wu Charles <wcadaydayup@163.com>
2019-07-24 17:49:09 +0800

    创建顾客更新定时任务

--- run_task.py
+++ run_task.py
@@ -185,7 +185,6 @@ def run():
     # 模板邮件定时发送任务
     tp.create_periodic_task(tmp.execute_email_task, seconds=120, interval=120)
 
-
     # shopify 定时更新任务, 请放在这下面
     sdp = ShopifyDataProcessor(db_info=db_info)
     tp.create_periodic_task(sdp.update_new_shopify, seconds=120, max_instances=50)   # 新店铺拉 产品类目 产品 订单 top_product
@@ -193,7 +192,8 @@ def run():
     tp.create_cron_task(sdp.update_shopify_product, "*", 12, 00)
     tp.create_cron_task(sdp.update_top_product, "*", 12, 00)
 
-
+    #　定时更新店铺的顾客信息
+    tp.create_periodic_task(sdp.update_shopify_customers, seconds=259200)
 
     # ems 定时更新任务请放在这下面
     ems = EMSDataProcessor("Leemon", "leemon.li@orderplus.com", db_info=db_info)
