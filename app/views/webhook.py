2de451fe24aa2d84a0f80226365881ae0036feed
shaowei <shaowei@shouxin168.com>
2019-07-30 18:06:36 +0800

    modify

--- app/views/webhook.py
+++ app/views/webhook.py
@@ -5,91 +5,59 @@ from rest_framework.views import APIView
 from app import models
 
 
-class EventCartUpdate(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ cat update------------:")
-        # print(type(request.META))
-        # store_url = request.META["HTTP_X_SHOPIFY_SHOP_DOMAIN"]
-        # print(store_url)
-        # print(type(request.data))
-        print(json.dumps(request.data))
-        # print(type(request.META))
-
-        return Response({"code": 200})
-
-
-class EventCartCreate(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ cat create ------------:")
-        # print(request.META, type(request.META))
-        print(json.dumps(request.data))
-        return Response({"code": 200})
-
-
-class EventOrderUpdate(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ order update------------:")
-        # print(request.META, type(request.META))
-        print(json.dumps(request.data))
-        return Response({"code": 200})
-
-
-class EventOrderCreate(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ order create ------------:")
-        print(json.dumps(request.data))
-        res = {}
-        store = models.Store.objects.filter(url=request.META["HTTP_X_SHOPIFY_SHOP_DOMAIN"]).first()
-        res["store"] = store
-        res["order_uuid"] = request.data["id"]
-        res["status"] = 0
-        res["total_price"] = request.data["total_price"]
-        create_time = request.data["created_at"].replace("T", " ")[:-6]
-        res["order_create_time"] = datetime.datetime.strptime(create_time, "%Y-%m-%d %H:%M:%S")
-        res["customer_uuid"] = request.data["customer"]["id"]
-        res["create_time"] = datetime.datetime.now()
-        order_instance = models.OrderEvent.objects.filter(store=store, order_uuid=request.data["id"]).first()
-        li = []
-        for item in request.data["line_items"]:
-            product_id = item["product_id"]
-            title = item["title"]
-            price = item["price"]
-            quantity = item["quantity"]
-            li.append({"product_id": product_id, "title": title, "price": price, "quantity": quantity})
-        res["product_info"] = json.dumps(li)
-        if order_instance:
-            order_instance.product_info = res["product_info"]
-            order_instance.total_price = res["total_price"]
-            order_instance.save()
-        else:
-            models.OrderEvent.objects.create(**res)
-        customer_instance = models.Customer.objects.filter(uuid=request.data["customer"]["id"]).first()
-        if not customer_instance:
-            customer_res = {}
-            customer_res["store"] = store
-            customer_res["uuid"] = request.data["customer"]["id"]
-            customer_res["customer_email"] = request.data["customer"]["email"]
-            customer_res["first_name"] = request.data["customer"]["first_name"]
-            customer_res["last_name"] = request.data["customer"]["last_name"]
-            customer_res["accept_marketing_status"] = request.data["customer"]["accepts_marketing"]
-            sign_up_time = request.data["customer"]["created_at"].replace("T"," ")[:-6]
-            customer_res["sign_up_time"] = datetime.datetime.strptime(sign_up_time, "%Y-%m-%d %H:%M:%S")
-            customer_res["last_order_status"] = 0
-            customer_res["last_order_id"] = request.data["customer"]["last_order_id"]
-            updated_at = request.data["customer"]["updated_at"].replace("T"," ")[:-6]
-            customer_res["last_order_time"] = datetime.datetime.strptime(updated_at, "%Y-%m-%d %H:%M:%S")
-            customer_res["create_time"] = datetime.datetime.now()
-            models.Customer.objects.create(**customer_res)
-        else:
-            customer_instance.last_order_status = 0
-            updated_at = request.data["customer"]["updated_at"].replace("T", " ")[:-6]
-            customer_instance.last_order_time = datetime.datetime.strptime(updated_at, "%Y-%m-%d %H:%M:%S")
-            customer_instance.save()
-        return Response({"code": 200})
+# class EventOrderCreate(APIView):
+#
+#     def post(self, request, *args, **kwargs):
+#         print("------------ order create ------------:")
+#         print(json.dumps(request.data))
+#         res = {}
+#         store = models.Store.objects.filter(url=request.META["HTTP_X_SHOPIFY_SHOP_DOMAIN"]).first()
+#         res["store"] = store
+#         res["order_uuid"] = request.data["id"]
+#         res["status"] = 0
+#         res["total_price"] = request.data["total_price"]
+#         create_time = request.data["created_at"].replace("T", " ")[:-6]
+#         res["order_create_time"] = datetime.datetime.strptime(create_time, "%Y-%m-%d %H:%M:%S")
+#         res["customer_uuid"] = request.data["customer"]["id"]
+#         res["create_time"] = datetime.datetime.now()
+#         order_instance = models.OrderEvent.objects.filter(store=store, order_uuid=request.data["id"]).first()
+#         li = []
+#         for item in request.data["line_items"]:
+#             product_id = item["product_id"]
+#             title = item["title"]
+#             price = item["price"]
+#             quantity = item["quantity"]
+#             li.append({"product_id": product_id, "title": title, "price": price, "quantity": quantity})
+#         res["product_info"] = json.dumps(li)
+#         if order_instance:
+#             order_instance.product_info = res["product_info"]
+#             order_instance.total_price = res["total_price"]
+#             order_instance.save()
+#         else:
+#             models.OrderEvent.objects.create(**res)
+#         customer_instance = models.Customer.objects.filter(uuid=request.data["customer"]["id"]).first()
+#         if not customer_instance:
+#             customer_res = {}
+#             customer_res["store"] = store
+#             customer_res["uuid"] = request.data["customer"]["id"]
+#             customer_res["customer_email"] = request.data["customer"]["email"]
+#             customer_res["first_name"] = request.data["customer"]["first_name"]
+#             customer_res["last_name"] = request.data["customer"]["last_name"]
+#             customer_res["accept_marketing_status"] = request.data["customer"]["accepts_marketing"]
+#             sign_up_time = request.data["customer"]["created_at"].replace("T"," ")[:-6]
+#             customer_res["sign_up_time"] = datetime.datetime.strptime(sign_up_time, "%Y-%m-%d %H:%M:%S")
+#             customer_res["last_order_status"] = 0
+#             customer_res["last_order_id"] = request.data["customer"]["last_order_id"]
+#             updated_at = request.data["customer"]["updated_at"].replace("T"," ")[:-6]
+#             customer_res["last_order_time"] = datetime.datetime.strptime(updated_at, "%Y-%m-%d %H:%M:%S")
+#             customer_res["create_time"] = datetime.datetime.now()
+#             models.Customer.objects.create(**customer_res)
+#         else:
+#             customer_instance.last_order_status = 0
+#             updated_at = request.data["customer"]["updated_at"].replace("T", " ")[:-6]
+#             customer_instance.last_order_time = datetime.datetime.strptime(updated_at, "%Y-%m-%d %H:%M:%S")
+#             customer_instance.save()
+#         return Response({"code": 200})
 
 
 class EventOrderPaid(APIView):
@@ -117,42 +85,6 @@ class EventOrderPaid(APIView):
         updated_at = request.data["customer"]["updated_at"].replace("T", " ")[:-6]
         order_instance.order_update_time = datetime.datetime.strptime(updated_at, "%Y-%m-%d %H:%M:%S")
         order_instance.save()
-        return Response({"code": 200})
-
-
-class EventOrderFulfilled(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ order Fulfilled ------------:")
-        # print(request.META, type(request.META))
-        print(json.dumps(request.data))
-        return Response({"code": 200})
-
-
-class EventOrderPartiallyFulfilled(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ order Partially Fulfilled------------:")
-        # print(request.META, type(request.META))
-        print(json.dumps(request.data))
-        return Response({"code": 200})
-
-
-class EventDraftOrdersCreate(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ DraftOrders  Create ------------:")
-        # print(request.META, type(request.META))
-        print(json.dumps(request.data))
-        return Response({"code": 200})
-
-
-class EventDraftOrdersUpdate(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ DraftOrders Update ------------:")
-        # print(request.META, type(request.META))
-        print(json.dumps(request.data))
         return Response({"code": 200})
 
 
@@ -292,20 +224,3 @@ class CheckoutsUpdate(APIView):
         checkout_instance.save()
         return Response({"code": 200})
 
-
-class CheckoutsFulfilled(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ Checkouts Fulfilled ------------:")
-        # print(request.META, type(request.META))
-        print(json.dumps(request.data))
-        return Response({"code": 200})
-
-
-class CheckoutDelete(APIView):
-
-    def post(self, request, *args, **kwargs):
-        print("------------ Checkout Delete ------------:")
-        # print(request.META, type(request.META))
-        print(json.dumps(request.data))
-        return Response({"code": 200})
