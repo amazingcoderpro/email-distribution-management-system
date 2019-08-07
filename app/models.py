a2fb11adc0e9c624646be9f1bc8d3bf469b3c9cc
Leemon7 <limengqiAliase@163.com>
2019-08-07 18:10:02 +0800

    modify

--- app/models.py
+++ app/models.py
@@ -245,7 +245,7 @@ class CustomerUnsubscribe(models.Model):
     unsubscribe_choices = ((0, 'is false'), (1, 'is true'), (2, 'is sleep'))
     unsubscribe_status = models.SmallIntegerField(db_index=True, choices=unsubscribe_choices, default=0, verbose_name="取消订阅或者休眠")
     unsubscribe_date = models.DateTimeField(blank=True, null=True, verbose_name="取消订阅时间/休眠的截止时间")  # unsubscribe_status
-    if True:
+    if ENABLE_MIGRATE:
         store_id = models.IntegerField(db_index=True, verbose_name="店铺id")
     else:
         store = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
@@ -253,7 +253,7 @@ class CustomerUnsubscribe(models.Model):
     update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
 
     class Meta:
-        # managed = ENABLE_MIGRATE
+        managed = ENABLE_MIGRATE
         db_table = 'customer_unsubscribe'
 
 
